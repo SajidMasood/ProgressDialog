9d24f3db2e98d35e466dbc4825e9fbead3c4c512
Mr-Abdali <sajidullah_m@yahoo.com>
2018-04-20 02:06:43 -0700

    Initial commit

---
+++ app/src/main/java/com/mr_abdali/progressdialog/MainActivity.java
@@ -0,0 +1,24 @@
+package com.mr_abdali.progressdialog;
+
+import android.app.ProgressDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    //
+    ProgressDialog dialog;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //
+        dialog = new ProgressDialog(MainActivity.this);
+        dialog.setTitle("Progress Dialog");
+        dialog.setMessage("Please Wait...");
+        dialog.show();
+    }
+}
